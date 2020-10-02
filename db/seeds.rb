e672fc29e4ea8713d05dad6f00f4bba8f29814b3
blumenthaler <blumenthaleralex@gmail.com>
2020-10-02 18:27:07 -0400

    fixed typos for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,11 @@ user_3 = User.create(username: "PacoJastorius", email: "bass_god@bloatedego.com"
 
 standard_1 = JazzStandard.create(title: "Autumn Leaves", composer: "Joseph Kosma", recording: "Somethin' Else", performer: "Cannonball Adderley", tempo: 120, levels_of_knowledge: "melody, lyrics, chords", date_memorized: "March 20, 2015", user_id: user_1.id)
 
-standard_2 = JazzStandard.create(title: "I Love You", composer: "Cole Porter", recording: "Live at The Vanguard Vol. 1", performer: "Bill Evans", tempo: 150, levels_of_knowledge: "melody, lyrics, chords, bassline", date_memorized: "April 1, 2018" user_id: user_2.id)
+standard_2 = JazzStandard.create(title: "I Love You", composer: "Cole Porter", recording: "Live at The Vanguard Vol. 1", performer: "Bill Evans", tempo: 150, levels_of_knowledge: "melody, lyrics, chords, bassline", date_memorized: "April 1, 2018", user_id: user_2.id)
 
-standard_3 = JazzStandard.create(title: "Beautiful Love", composer: "Victor Young", recording: "Live Somewhere Else", performer: "Bill Evans", tempo: 130, levels_of_knowledge: "melody, lyrics, chords", date_memorized: "January 10, 2007" user_id: user_2.id)
+standard_3 = JazzStandard.create(title: "Beautiful Love", composer: "Victor Young", recording: "Live Somewhere Else", performer: "Bill Evans", tempo: 130, levels_of_knowledge: "melody, lyrics, chords", date_memorized: "January 10, 2007", user_id: user_2.id)
 
-standard_4 = JazzStandard.create(title: "Blue Daniel", composer: "Frank Rosolino", recording: "Another Record", performer: "Cannonball Adderley", tempo: 130, levels_of_knowledge: "melody, chords, bassline", date_memorized: "February 10, 2019" user_id: user_1.id)
+standard_4 = JazzStandard.create(title: "Blue Daniel", composer: "Frank Rosolino", recording: "Another Record", performer: "Cannonball Adderley", tempo: 130, levels_of_knowledge: "melody, chords, bassline", date_memorized: "February 10, 2019", user_id: user_1.id)
 
 standard_5 = JazzStandard.create(title: "Anthropology", composer: "Charlie Parker", recording: "One of those Records Live", performer: "Charlie Parker", tempo: 140, levels_of_knowledge: "melody, chords, bassline", date_memorized: "March 20, 2015", user_id: user_3.id)
 
