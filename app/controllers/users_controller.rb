11cdd916b4bfcaf745624c239970f259f60bd94b
blumenthaler <blumenthaleralex@gmail.com>
2020-10-05 17:56:02 -0400

    finished login form/routes, starting user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,19 @@ class UsersController < ApplicationController
         erb :login
     end 
 
+    post '/login' do
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            binding.pry
+            redirect "/users/#{@user.id}"
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/users/:id' do
+        @user = User.find_by(id: params[:user_id])
+        erb :'users/show'
+    end
 end
