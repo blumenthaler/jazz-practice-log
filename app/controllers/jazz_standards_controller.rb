d63723609147e4b76a95e97106c5fdf4634cbb90
blumenthaler <blumenthaleralex@gmail.com>
2020-10-06 16:38:29 -0400

    fixed post standards route to make association w/ user

--- app/controllers/jazz_standards_controller.rb
+++ app/controllers/jazz_standards_controller.rb
@@ -12,7 +12,16 @@ class JazzStandardsController < ApplicationController
 
     post '/jazz-standards' do
         binding.pry
-        @standard = JazzStandard.create(params)
+        @standard = JazzStandard.create(
+            title: params[:title],
+            composer: params[:composer],
+            recording: params[:recording],
+            peformer: params[:performer],
+            tempo: params[:tempo],
+            levels_of_knowledge: params[:levels_of_knowledge],
+            date_memorized: params[:date_memorized],
+            user_id: current_user.id
+            )
         redirect "/jazz-standards/#{@standard.id}"
     end
 
