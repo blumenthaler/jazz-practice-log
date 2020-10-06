ef1918a6179d943aec72026d7b52f0f32ed417fe
blumenthaler <blumenthaleralex@gmail.com>
2020-10-06 14:16:38 -0400

    added signup routes/form + logout route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
     get '/login' do
-        erb :login
+        erb :'/users/login'
     end 
 
     post '/login' do
@@ -15,8 +15,22 @@ class UsersController < ApplicationController
     end
 
     get '/users/:id' do
-        binding.pry
         @user = current_user
-        erb :'users/show'
+        erb :'/users/show'
+    end
+
+    get '/signup' do
+        erb :'/users/signup'
+    end
+
+    post '/users' do
+        @user = User.create(params)
+        session[:user_id] = @user.id
+        redirect "/users/#{@user.id}"
+    end
+
+    get '/logout' do
+        session.clear
+        redirect '/login'
     end
 end
